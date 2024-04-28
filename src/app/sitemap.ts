5efb3f749269d9ab9523c9f80c1b91b2fbb4c2df
Daniel Xavier <dxdanielxavier@gmail.com>
2024-04-28 18:08:17 -0300

    chore: remove useless files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://danielxavier.dev",
+      url: "https://www.danielxavier.dev",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
