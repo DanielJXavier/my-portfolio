c65f644b7fba3c49c0eea5d5c7947617f8e985bc
Daniel Xavier <dxdanielxavier@gmail.com>
2024-04-30 15:31:29 -0300

    feat: add header desktop styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="py-4 border-white border-solid border-b">
+    <header className="py-4 shadow-sm shadow-[#bbb]">
       <div className="container mx-auto">
-        <Link className="text-2xl antialiased font-light" href="/">
+        <Link
+          className="text-2xl font-light hover:text-[#fff] hover:font-normal"
+          href="/"
+        >
           &lt;DanielXavier /&gt;
         </Link>
       </div>
