d158273c21d24ce0f2bd337ab2e3ba4a96d49f5e
Daniel Xavier <dxdanielxavier@gmail.com>
2024-04-30 17:15:46 -0300

    feat: add a copyright footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import "./globals.css";
 
 const font = Source_Code_Pro({ subsets: ["latin"] });
 
+const year = new Date().getFullYear();
+
 export const metadata: Metadata = {
   title: "<DanielXavier />",
   description: "Technical Lead | JavaScript | React | MBA, Team Leadership",
@@ -20,9 +22,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>
+      <body
+        className={`${font.className} relative pb-24 md:pb-20 lg:pb-16 lg:min-h-dvh`}
+      >
         <Header />
         {children}
+        <footer className="absolute right-0 bottom-0 left-0 h-24 md:h-20 lg:h-16 flex items-center justify-center">
+          <p className="text-xs md:text-sm text-center">
+            Copyright &copy; {year} Daniel Xavier. <br className="sm:hidden" />
+            All Rights Reserved.
+          </p>
+        </footer>
       </body>
     </html>
   );
