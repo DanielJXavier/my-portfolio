3746df40f3b00e8b3841b2b8d6979ed9b9f39b19
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-01 19:35:47 -0300

    fix: remove useless space

--- src/app/experience/components/ExperienceItem.tsx
+++ src/app/experience/components/ExperienceItem.tsx
@@ -24,7 +24,7 @@ export default function ExperienceItem({
   const experienceView = useContext(ExperienceViewContext);
 
   return (
-    <article className="my-6 sm:my-10 relative before:content-[''] before:absolute before:top-[43px] before:bottom-[-21px] before:sm:bottom-[-36px] last-of-type:before:bottom-0 before:left-[16px] before:ml-0.5 before:w-1 before:bg-gradient-to-b before:from-[#6d0ad5] before:via-[#6d0ad5] before:to-[#000] before:-z-10">
+    <article className="my-6 sm:my-10 relative before:content-[''] before:absolute before:top-[40px] before:bottom-[-21px] before:sm:bottom-[-36px] last-of-type:before:bottom-0 before:left-[16px] before:ml-0.5 before:w-1 before:bg-gradient-to-b before:from-[#6d0ad5] before:via-[#6d0ad5] before:to-[#000] before:-z-10">
       <header className="flex items-start gap-x-2">
         <Image
           className="rounded-full"
