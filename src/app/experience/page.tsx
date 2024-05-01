02603535a092bac989f3ab4b2031dca202cdfa51
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-01 17:10:22 -0300

    feat: add Experience page

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,9 @@
+export default function Experience() {
+  return (
+    <>
+      <main className="container mx-auto px-4 md:px-5 xl:px-6 pt-12">
+        <h1 className="text-3xl">Experience</h1>
+      </main>
+    </>
+  );
+}
