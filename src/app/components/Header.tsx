bcb62d0c8850990ef4d13f298e28676c7a29d021
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-02 21:17:26 -0300

    feat: fix header grid

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
 
   return (
     <header className="py-2 md:py-3 lg:py-4 shadow-sm shadow-[#bbb]">
-      <div className="relative container mx-auto px-4 md:px-5 xl:px-6 grid grid-cols-[1fr_auto] items-center">
+      <div className="relative container mx-auto px-4 md:px-5 xl:px-6 grid grid-cols-[auto_auto] items-center justify-between">
         <Link
           className={`text-xl md:text-2xl lg:text-3xl font-light hover:text-[#fff] ${
             pathname === "/" ? "text-[#fff] cursor-default" : ""
