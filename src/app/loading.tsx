4251e86dc65aa525764f452ae2b2fd1157362be6
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-02 22:20:00 -0300

    feat: add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main className="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center">
+      <div className="loading">
+        <p>dx</p>
+      </div>
+    </main>
+  );
+}
