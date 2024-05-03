ebf92133886aee4cc2ba66f5917bc4ffdb271731
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-03 11:50:22 -0300

    feat: add main-container class

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -3,9 +3,5 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <main className="container mx-auto px-4 md:px-5 xl:px-6 pt-6 md:pt-8 xl:pt-12">
-      {children}
-    </main>
-  );
+  return <main className="main-container">{children}</main>;
 }
