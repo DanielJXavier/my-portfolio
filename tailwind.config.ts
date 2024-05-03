ac8bab947436b6a7342210c59c539ab54c783ff8
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-03 11:49:35 -0300

    feat: add custom shaped dropShadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/app/**/*.tsx"],
+  theme: {
+    extend: {
+      dropShadow: {
+        shaped: [
+          "2px 0 0 #bbb",
+          "0 2px 0 #bbb",
+          "-2px 0 0 #bbb",
+          "0 -2px 0 #bbb",
+        ],
+      },
+    },
+  },
 };
 
 export default config;
