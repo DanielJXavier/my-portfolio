b4350a470db3596d62c00ac6707d263f1bdaf962
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-03 11:56:56 -0300

    fix: change loading postion

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <main className="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center">
+    <main className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
       <div className="loading">
         <p>dx</p>
       </div>
