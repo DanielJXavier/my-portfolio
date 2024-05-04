78d75c1b5a41dfea1dab7e7193fda776442082c9
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-03 21:58:29 -0300

    fix: remove main title margin

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -7,7 +7,7 @@ type TitleProps = Readonly<{
 
 export default function Title({ children, icon }: TitleProps) {
   return (
-    <h1 className="text-xl md:text-2xl lg:text-3xl mb-4 md:mb-6 xl:mb-8 flex items-center gap-x-2">
+    <h1 className="text-xl md:text-2xl lg:text-3xl flex items-center gap-x-2">
       {icon}
       {children}
     </h1>
