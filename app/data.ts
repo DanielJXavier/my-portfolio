787b3c0a77e7b00ee14dfd7285478da3ae90e595
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-04 00:45:38 -0300

    refactor: add some type definitions

--- app/data.ts
+++ app/data.ts
@@ -1,4 +1,8 @@
-export const author = {
+type AuthorType = {
+  [key: string]: string;
+};
+
+export const author: AuthorType = {
   firstName: "Daniel",
   lastName: "Xavier",
   headline: "Technical Lead | JavaScript | React | MBA, Team Leadership",
