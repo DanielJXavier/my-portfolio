36eb0f529295157f417bd71c0acc11eb2a0b62e0
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-04 00:22:13 -0300

    feat: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,8 +5,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://www.danielxavier.dev",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "daily",
       priority: 1,
+    },
+    {
+      url: "https://www.danielxavier.dev/experience",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.danielxavier.dev/education",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.danielxavier.dev/skills",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.danielxavier.dev/recommendations",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.5,
     },
   ];
 }
