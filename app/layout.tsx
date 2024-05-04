fc204236e805a382b3dd44feb2bbdcc1b8ed688f
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-04 02:18:33 -0300

    feat: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ReactNode } from "react";
 
@@ -40,6 +41,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </p>
         </footer>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
