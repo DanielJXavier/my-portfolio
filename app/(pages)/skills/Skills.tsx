f078f8655ad2e9aee63a71097828968a7dcb6406
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-07 19:27:30 -0300

    fix: improve the code to prevent CLS in skills page

--- app/(pages)/skills/Skills.tsx
+++ app/(pages)/skills/Skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import Title from "@/components/Title";
 import Psychology from "@/icons/Psychology";
@@ -9,7 +9,7 @@ import Menu from "../components/Menu";
 
 import shuffleSkills from "./utils/shuffle";
 
-import { hardSkills, softSkills, skillType } from "./data";
+import { skills } from "./data";
 
 const mapSizeToFontSize = {
   1: "text-sm md:text-base lg:text-lg",
@@ -20,40 +20,28 @@ const mapSizeToFontSize = {
 };
 
 export default function Skills() {
-  const [skillsList, setSkillsList] = useState<skillType[]>([]);
-  const [skill, setSkill] = useState("all");
-
-  useEffect(() => {
-    switch (skill) {
-      case "hard":
-        setSkillsList(hardSkills);
-        break;
-      case "soft":
-        setSkillsList(softSkills);
-        break;
-      default:
-        setSkillsList([...hardSkills, ...softSkills]);
-        break;
-    }
-  }, [skill]);
+  const [skillFilter, setSkillFilter] = useState("all");
 
   return (
     <>
       <Title icon={<Psychology />}>Skills</Title>
       <Menu
         items={["all", "hard", "soft"]}
-        activeItem={skill}
-        setActiveItem={setSkill}
+        activeItem={skillFilter}
+        setActiveItem={setSkillFilter}
       />
       <article className="my-6 md:my-8 lg:my-10 flex gap-x-3 md:gap-x-5 lg:gap-x-8 gap-y-2 md:gap-y-3 lg:gap-y-4 flex-wrap items-center justify-center lg:justify-between">
-        {shuffleSkills(skillsList).map(({ name, size }, i) => (
-          <span
-            className={`${mapSizeToFontSize[size]} font-light tracking-tighter [word-spacing:-0.35em]`}
-            key={i}
-          >
-            {name}
-          </span>
-        ))}
+        {shuffleSkills(skills)
+          .filter(({ type }) => skillFilter === "all" || type === skillFilter)
+          .map(({ name, size }, i) => (
+            <span
+              className={`${mapSizeToFontSize[size]} font-light tracking-tighter [word-spacing:-0.35em]`}
+              key={i}
+              suppressHydrationWarning
+            >
+              {name}
+            </span>
+          ))}
       </article>
       <article className="py-5 md:py-6 lg:py-8 text-base md:text-lg lg:text-2xl font-light text-center lg:text-justify">
         And always learning new things to expand the list...
