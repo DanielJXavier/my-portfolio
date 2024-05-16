263c0cd2c0f0db93ed3ca4c4d73a13c1fadc4dec
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-16 11:48:44 -0300

    fix: change home summary

--- app/(home)/data.ts
+++ app/(home)/data.ts
@@ -1,10 +1,11 @@
 type ParagraphsType = string[];
 
 export const paragraphs: ParagraphsType = [
-  "With a decade-long web development journey and an MBA in strategic people management, I've honed my skills to become a dedicated technical leader.",
-  "I aim to craft elegant solutions to intricate problems, leveraging my expertise in JavaScript and its associated frameworks and libraries such as React.js and Node.js, which I have mastered over the years. I also love teaching and helping others, so I led and mentored teams of talented engineers who work on the search and recommendation systems and the design system of the OLX platform, the global leader in online classifieds.",
-  "In my past roles, I've acquired and applied pivotal management skills such as emotional intelligence, collaborative design, and agile methodologies. These skills have not only given me a competitive edge but also shaped me into a well-rounded professional. I am driven by an insatiable curiosity to learn and a readiness to take on new challenges. I firmly believe in the transformative power of technology and its potential to positively impact society.",
-  "Besides technology, I find joy in capturing the beauty of landscapes through my lens and staying active with sports. I am always open to exploring new opportunities and forging meaningful connections. Let's engage in a conversation!",
+  "With a decade-long web development journey and an MBA in strategic people management, I have honed my skills to become a dedicated technical leader, balancing hard and soft skills to perform beyond expectations.",
+  "My passion is creating elegant solutions to complex problems, leveraging my experience in JavaScript and its associated frameworks and libraries, such as React.js and Node.js, which I have mastered over the years.",
+  "I also love teaching, inspiring, and helping others, so I've dedicated my last few years to leading and mentoring teams of talented engineers working on complex projects like search and recommendation systems and the Design System of the OLX platform, a global leader in online classifieds.",
+  "In these previous roles, I acquired and applied management skills such as emotional intelligence, conflict resolution, resilience, and also agile methodologies. These skills not only gave me a competitive edge but also transformed me into a well-rounded professional. I am driven by an insatiable curiosity to learn new things and a willingness to face new challenges.",
+  "In addition to technology, I like to photograph landscapes, practice adventure sports, and visit new places whenever possible. I'm always open to exploring new opportunities and making meaningful connections. Let's talk!",
 ];
 
 type LinksType = {
