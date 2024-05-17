c9ba2f80d8807e21ce6b66ca977ffa2a829456b2
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-17 19:46:44 -0300

    tests: force shuffle skills util predictable behavior for tests

--- app/(pages)/skills/utils/shuffle.test.ts
+++ app/(pages)/skills/utils/shuffle.test.ts
@@ -3,9 +3,27 @@ import shuffle from "./shuffle";
 import { skills } from "../data";
 
 describe("shuffle util", () => {
-  it("returns a shuffled list", () => {
+  const NODE_ENV_ORIGINAL = process.env.NODE_ENV;
+
+  afterAll(() => {
+    process.env = Object.assign(process.env, {
+      NODE_ENV: NODE_ENV_ORIGINAL,
+    });
+  });
+
+  it("returns the given list in test environment", () => {
     const shuffledSkills = shuffle(skills);
 
-    expect(skills).not.toContainEqual(shuffledSkills);
+    expect(skills).toEqual(shuffledSkills);
+  });
+
+  it("returns a shuffled list", () => {
+    process.env = Object.assign(process.env, {
+      NODE_ENV: "production",
+    });
+
+    const shuffledSkills = shuffle(skills);
+
+    expect(skills).not.toStrictEqual(shuffledSkills);
   });
 });
--- app/(pages)/skills/utils/shuffle.ts
+++ app/(pages)/skills/utils/shuffle.ts
@@ -1,11 +1,16 @@
 import { skillType } from "../data";
 
 export default function shuffle(array: skillType[]) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-
-    [array[i], array[j]] = [array[j], array[i]];
+  if (process.env.NODE_ENV === "test") {
+    return array;
   }
 
-  return array;
+  const shuffled = JSON.parse(JSON.stringify(array));
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
 }
