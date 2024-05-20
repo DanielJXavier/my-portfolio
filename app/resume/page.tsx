8b5bc974170f806eff994147eee5172f592e031a
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-20 19:24:38 -0300

    feat: add the resume page content

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from "react";
-
 import { author } from "@/data";
 import { links, paragraphs } from "@/(pages)/(home)/data";
 import { experience } from "@/(pages)/(content)/experience/data";
@@ -8,7 +6,7 @@ import { skills } from "@/(pages)/(content)/skills/data";
 
 export default function Page() {
   return (
-    <main className="w-[210mm] h-[297mm] font-serif p-[0.5in]">
+    <main className="w-[210mm] flex flex-col gap-y-4 font-serif p-[0.5in]">
       <section>
         <h1 className="text-[18pt] font-bold">
           {author.firstName} {author.lastName}
@@ -34,7 +32,7 @@ export default function Page() {
           </a>
         </p>
       </section>
-      <section className="mt-4">
+      <section>
         <h2 className="text-[16pt] font-bold">Summary</h2>
         {paragraphs
           .filter(({ resume }) => resume)
@@ -44,55 +42,68 @@ export default function Page() {
             </p>
           ))}
       </section>
-      <section className="mt-4">
-        <h2 className="text-[16pt] font-bold">Experience</h2>
-        {experience
-          .filter(({ resume }) => resume)
-          .map(
-            (
-              { role, companyName, year, resposibilities, greatestChallenge },
-              i
-            ) => (
-              <Fragment key={i}>
-                <p className="text-[12pt] font-bold">
-                  {role} @ {companyName}
-                </p>
-                <p className="text-[12pt]">{year}</p>
-                <ul>
-                  {resposibilities
-                    .filter(({ resume }) => resume)
-                    .map(({ text }, i) => (
-                      <li key={i} className="text-[12pt] text-justify">
-                        {text}
-                      </li>
-                    ))}
-                </ul>
-                <p className="text-[12pt] text-justify">{greatestChallenge}</p>
-              </Fragment>
-            )
-          )}
-      </section>
-      <section className="mt-4">
-        <h2 className="text-[16pt] font-bold">Education</h2>
-        {education.map(({ schoolName, fieldOfStudy, degree, years }, i) => (
-          <Fragment key={i}>
-            <p className="text-[12pt] font-bold">
-              {degree}, {fieldOfStudy}
-            </p>
-            <p className="text-[12pt]">{schoolName}</p>
-            <p className="text-[12pt]">{years}</p>
-          </Fragment>
-        ))}
-      </section>
-      <section className="mt-4">
+      <section>
         <h2 className="text-[16pt] font-bold">Skills</h2>
-        {skills
-          .filter(({ size }) => size >= 4)
-          .map(({ name }, i) => (
-            <Fragment key={i}>
-              <p className="text-[12pt]">{name}</p>
-            </Fragment>
+        <ul className="flex gap-x-3.5 flex-wrap">
+          {skills
+            .filter(({ resume }) => resume)
+            .map(({ name }, i) => (
+              <li
+                key={i}
+                className="relative text-[12pt] after:content-['•'] after:absolute after:-right-[10px] last-of-type:after:content-['']"
+              >
+                {name}
+              </li>
+            ))}
+        </ul>
+      </section>
+      <section>
+        <h2 className="text-[16pt] font-bold">Experience</h2>
+        <div className="flex flex-col gap-y-3">
+          {experience
+            .filter(({ resume }) => resume)
+            .map(
+              (
+                { role, companyName, year, responsibilities, biggestChallenge },
+                i
+              ) => (
+                <div key={i}>
+                  <p className="text-[12pt] font-bold">
+                    {role} @ {companyName}
+                  </p>
+                  <p className="text-[12pt]">{year}</p>
+                  <ul className="pl-3.5">
+                    {responsibilities
+                      .filter(({ resume }) => resume)
+                      .map(({ text }, i) => (
+                        <li
+                          key={i}
+                          className="relative text-[12pt] text-justify before:content-['•'] before:absolute before:-left-[10px]"
+                        >
+                          {text}
+                        </li>
+                      ))}
+                  </ul>
+                  <p className="text-[12pt] text-justify">
+                    Biggest challenge: {biggestChallenge}
+                  </p>
+                </div>
+              )
+            )}
+        </div>
+      </section>
+      <section>
+        <h2 className="text-[16pt] font-bold">Education</h2>
+        <div className="flex flex-col gap-y-3">
+          {education.map(({ schoolName, fieldOfStudy, degree }, i) => (
+            <div key={i}>
+              <p className="text-[12pt] font-bold">
+                {degree}, {fieldOfStudy}
+              </p>
+              <p className="text-[12pt]">{schoolName}</p>
+            </div>
           ))}
+        </div>
       </section>
     </main>
   );
