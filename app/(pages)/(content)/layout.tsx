86e9ff4924f189fc5f066316b2909f4154bd03db
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-19 23:48:03 -0300

    Update layout.tsx

--- app/(pages)/(content)/layout.tsx
+++ app/(pages)/(content)/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header />
-      <main className="main-container">{children}</main>;
+      <main className="main-container">{children}</main>
       <footer className="absolute right-0 bottom-0 left-0 h-24 md:h-20 lg:h-16 flex items-center justify-center">
         <p className="text-xs md:text-sm text-center">
           Copyright &copy; {year} {author.firstName} {author.lastName}.{" "}
