db010d968b4bc2cfe308153d38dac0b1a633e4dc
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-20 19:24:50 -0300

    tests: add the resume page tests

---
+++ app/resume/page.test.tsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+
+import { render } from "@testing-library/react";
+
+import Page from "./page";
+
+describe("Resume page", () => {
+  it("renders the page", () => {
+    const { container } = render(<Page />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
