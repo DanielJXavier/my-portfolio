58e1b62d6ce39ec406cce22fd65e1b87f5f75daf
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-22 02:26:04 -0300

    ci: change failureThreshold to 0.1

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,5 +4,5 @@ import { addMatchImageSnapshotCommand } from "@simonsmith/cypress-image-snapshot
 
 addMatchImageSnapshotCommand({
   customSnapshotsDir: "cypress/visual-testing",
-  failureThreshold: 0.05,
+  failureThreshold: 0.1,
 });
