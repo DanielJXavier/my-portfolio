f8479665c51311a91449a4d12a5af177a51016ff
Daniel Xavier <dxdanielxavier@gmail.com>
2024-05-30 16:47:45 -0300

    fix: add missing packages

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-import { i18n } from "./i18n-config";
-
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
-function getLocale(request: NextRequest): string | undefined {
+import { i18n } from "./i18n-config";
+
+const getLocale = (request: NextRequest): string | undefined => {
   const negotiatorHeaders: Record<string, string> = {};
 
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
@@ -18,7 +18,7 @@ function getLocale(request: NextRequest): string | undefined {
   const locale = matchLocale(languages, i18n.locales, i18n.defaultLocale);
 
   return locale;
-}
+};
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
