539e86c56aa517a4cb87c342dd3885dd0803f97a
Daniel Xavier <dxdanielxavier@gmail.com>
2024-06-03 10:55:54 -0300

    tests: add some tests for resume page

--- app/[lang]/resume/Resume.test.tsx
+++ app/[lang]/resume/Resume.test.tsx
@@ -4,6 +4,8 @@ import { render } from "@testing-library/react";
 
 import Page from "./page";
 
+import en from "dictionaries/en.json";
+
 import { useParams } from "next/navigation";
 
 jest.mock("next/navigation");
@@ -15,5 +17,30 @@ describe("Resume page (Client-side)", () => {
     const { container } = render(<Page />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("changes the page title when prints the page", () => {
+    const {
+      global: { author },
+    } = en;
+
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+
+    render(<Page />);
+
+    const beforeprint = new Event("beforeprint");
+    window.dispatchEvent(beforeprint);
+
+    expect(document.title).toEqual(
+      `Resume_${author.firstName}-${author.lastName}_${year}${month}${day}`
+    );
+
+    const afterprint = new Event("afterprint");
+    window.dispatchEvent(afterprint);
+
+    expect(document.title).toEqual("");
   });
 });
