0c869847a2f79b6b32feef028cde4eea1355c7ab
Daniel Xavier <dxdanielxavier@gmail.com>
2024-06-12 16:56:03 -0300

    feat: add editable textarea for summary

--- app/[lang]/resume/_components/Summary.tsx
+++ app/[lang]/resume/_components/Summary.tsx
@@ -4,25 +4,31 @@ import { getDictionary } from "get-dictionary";
 import { Lang } from "i18n-config";
 
 import { maxSummaryParagraphs } from "../_config";
+import { useRef } from "react";
 
 export default function Summary() {
   const { lang } = useParams<{ lang: Lang }>();
 
   const {
-    home: { summary },
+    home: { summary: defaultSummary },
     resume: { sectionTitles },
   } = getDictionary(lang);
+
+  const summary = useRef(
+    defaultSummary.filter((_, i) => i < maxSummaryParagraphs)
+  );
 
   return (
     <section>
       <h2 className="text-[16pt] font-bold">{sectionTitles.summary}</h2>
-      {summary
-        .filter((_, i) => i < maxSummaryParagraphs)
-        .map((paragraph, i) => (
-          <p key={`paragraph-${i}`} className="text-[12pt] text-justify">
-            {paragraph}
-          </p>
-        ))}
+      <textarea
+        className="w-full text-[12pt] text-justify align-top border border-primary print:border-none print:resize-none"
+        defaultValue={summary.current.join("\n")}
+        rows={summary.current.reduce(
+          (rows, paragraph) => rows + Math.ceil(paragraph.length / 8 / 12),
+          0
+        )}
+      />
     </section>
   );
 }
