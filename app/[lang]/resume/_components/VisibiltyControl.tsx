55022f6412344cf8b1a6c07275abbeeaf0c8bc84
Daniel Xavier <dxdanielxavier@gmail.com>
2024-06-13 18:08:16 -0300

    feat: add dynamic html tag prop in VisibilityControl

--- app/[lang]/resume/_components/VisibiltyControl.tsx
+++ app/[lang]/resume/_components/VisibiltyControl.tsx
@@ -1,30 +1,32 @@
-import { ReactNode, useState } from "react";
+import { createElement, ReactNode, useState } from "react";
 
 export default function VisibilityControl({
   children,
   className,
   defaultVisible,
+  tag = "div",
 }: {
   children: ReactNode;
   className?: string;
   defaultVisible?: boolean;
+  tag?: keyof HTMLElementTagNameMap;
 }) {
   const [visible, setVisible] = useState(defaultVisible ?? true);
 
-  return (
-    <div
-      className={`${className ?? ""} flex gap-x-1.5 ${
+  return createElement(
+    tag,
+    {
+      className: `${className ?? ""} flex gap-x-1.5 ${
         !visible ? "opacity-50 line-through print:hidden" : ""
-      }`}
-      data-testid="visibility-control"
-    >
-      <input
-        className="print:hidden"
-        type="checkbox"
-        checked={visible}
-        onChange={() => setVisible(!visible)}
-      />
-      {children}
-    </div>
+      }`,
+      "data-testid": "visibility-control",
+    },
+    <input
+      className="print:hidden"
+      type="checkbox"
+      checked={visible}
+      onChange={() => setVisible(!visible)}
+    />,
+    children
   );
 }
