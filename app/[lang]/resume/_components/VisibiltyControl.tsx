0ec5e1a3a9d82f295327648df00bdcb750fd94f8
Daniel Xavier <dxdanielxavier@gmail.com>
2024-06-13 18:07:42 -0300

    feat: improve VisibilityControl styles

--- app/[lang]/resume/_components/VisibiltyControl.tsx
+++ app/[lang]/resume/_components/VisibiltyControl.tsx
@@ -2,24 +2,29 @@ import { ReactNode, useState } from "react";
 
 export default function VisibilityControl({
   children,
+  className,
+  defaultVisible,
 }: {
   children: ReactNode;
+  className?: string;
+  defaultVisible?: boolean;
 }) {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(defaultVisible ?? true);
 
   return (
-    <div className="flex gap-x-1.5" data-testid="visibility-control">
+    <div
+      className={`${className ?? ""} flex gap-x-1.5 ${
+        !visible ? "opacity-50 line-through print:hidden" : ""
+      }`}
+      data-testid="visibility-control"
+    >
       <input
         className="print:hidden"
         type="checkbox"
         checked={visible}
         onChange={() => setVisible(!visible)}
       />
-      <div
-        className={`${!visible ? "opacity-50 line-through print:hidden" : ""}`}
-      >
-        {children}
-      </div>
+      {children}
     </div>
   );
 }
