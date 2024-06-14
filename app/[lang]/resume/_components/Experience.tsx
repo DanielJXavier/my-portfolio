69dd05dbbccedf9c859e174e7f74eb5d1a2d7bce
Daniel Xavier <dxdanielxavier@gmail.com>
2024-06-13 22:24:53 -0300

    feat: add VisibilityControl in experience section

--- app/[lang]/resume/_components/Experience.tsx
+++ app/[lang]/resume/_components/Experience.tsx
@@ -1,11 +1,11 @@
-import { useRef } from "react";
-
 import { useParams } from "next/navigation";
 
 import { getDictionary } from "get-dictionary";
 import { Lang } from "i18n-config";
 
 import { experience } from "@/[lang]/(pages)/experience/_config";
+
+import VisibilityControl from "./VisibiltyControl";
 
 export default function Experience() {
   const { lang } = useParams<{ lang: Lang }>();
@@ -15,44 +15,55 @@ export default function Experience() {
     resume: { sectionTitles, biggestChallengeTitle },
   } = getDictionary(lang);
 
-  const resumeExperience = useRef(experience.filter(({ resume }) => resume));
-
   return (
     <section>
       <h2 className="text-[16pt] font-bold">{sectionTitles.experience}</h2>
       <ul className="flex flex-col gap-y-3">
-        {resumeExperience.current.map(
-          ({ key, companyName, year, resumeResponsibilities }) => (
-            <li key={key}>
-              <p className="text-[12pt] font-bold">
-                {experienceStrings[key].role} @ {companyName}
-              </p>
-              <p className="text-[12pt]">{year}</p>
-              <ul className="pl-3.5 [&_li]:relative [&_li]:text-[12pt] [&_li]:text-justify [&_li]:before:content-['•'] [&_li]:before:absolute [&_li]:before:-left-[10px]">
-                {experienceStrings[key].responsibilities
-                  .filter((_, i) =>
-                    resumeResponsibilities?.some(
-                      (resumeResponsibility) => resumeResponsibility === i + 1
+        {experience.map(
+          ({ key, companyName, year, resumeResponsibilities, resume }) => (
+            <VisibilityControl
+              key={key}
+              className="items-baseline"
+              defaultVisible={resume}
+              tag="li"
+            >
+              <div>
+                <p className="text-[12pt] font-bold">
+                  {experienceStrings[key].role} @ {companyName}
+                </p>
+                <p className="text-[12pt]">{year}</p>
+                <ul className="pl-3.5 [&_li]:relative [&_li]:text-[12pt] [&_li]:text-justify [&_li]:before:content-['•'] [&_li]:before:absolute [&_li]:before:-left-[10px]">
+                  {experienceStrings[key].responsibilities.map(
+                    (responsibility, i) => (
+                      <VisibilityControl
+                        key={`responsibility-${i}`}
+                        className="items-baseline"
+                        defaultVisible={resumeResponsibilities?.some(
+                          (resumeResponsibility) =>
+                            resumeResponsibility === i + 1
+                        )}
+                        tag="li"
+                      >
+                        {responsibility}
+                      </VisibilityControl>
                     )
-                  )
-                  .map((responsibility, i) => (
-                    <li key={`responsibility-${i}`}>{responsibility}</li>
-                  ))}
-                <li>
-                  {biggestChallengeTitle}:{" "}
-                  {experienceStrings[key].biggestChallenge}
-                </li>
-              </ul>
-            </li>
+                  )}
+                  <VisibilityControl
+                    className="items-baseline"
+                    defaultVisible={resume}
+                    tag="li"
+                  >
+                    {biggestChallengeTitle}:{" "}
+                    {experienceStrings[key].biggestChallenge}
+                  </VisibilityControl>
+                </ul>
+              </div>
+            </VisibilityControl>
           )
         )}
-        {experience.length > resumeExperience.current.length && (
-          <li>
-            {previousItemsText.prefix}{" "}
-            {experience.length - resumeExperience.current.length}{" "}
-            {previousItemsText.sufix}
-          </li>
-        )}
+        <li>
+          {previousItemsText.prefix} {previousItemsText.sufix}
+        </li>
       </ul>
     </section>
   );
