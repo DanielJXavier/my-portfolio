763c61b17a4eee368f137b00567e9e5104e303c5
Daniel Xavier <dxdanielxavier@gmail.com>
2024-06-18 10:08:12 -0300

    fix: change summary paragraphs layout in resume

--- app/[lang]/resume/_components/Summary.tsx
+++ app/[lang]/resume/_components/Summary.tsx
@@ -23,9 +23,12 @@ export default function Summary() {
       <h2 className="text-[16pt] font-bold">{sectionTitles.summary}</h2>
       <textarea
         className="w-full text-[12pt] text-justify align-top border border-primary print:border-none print:resize-none"
-        defaultValue={summary.current.join("\n")}
+        defaultValue={summary.current.join("\n\n")}
         rows={summary.current.reduce(
-          (rows, paragraph) => rows + Math.ceil(paragraph.length / 8 / 12),
+          (rows, paragraph) =>
+            rows +
+            (summary.current.length - 1) +
+            Math.ceil(paragraph.length / 160),
           0
         )}
       />
