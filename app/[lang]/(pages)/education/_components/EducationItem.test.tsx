d69a5afdd081e323085a1470eac1c9cbb01d4251
Daniel Xavier <dxdanielxavier@gmail.com>
2024-06-26 14:12:29 -0300

    tests: change some tests to snapshots

--- app/[lang]/(pages)/education/_components/EducationItem.test.tsx
+++ app/[lang]/(pages)/education/_components/EducationItem.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import EducationItem from "./EducationItem";
 
@@ -17,7 +17,7 @@ describe("EducationItem component", () => {
   it("renders the component", () => {
     const { schoolId, schoolName, years } = education[0];
 
-    render(
+    const { container } = render(
       <EducationItem
         schoolId={schoolId}
         schoolName={schoolName}
@@ -29,26 +29,6 @@ describe("EducationItem component", () => {
       />
     );
 
-    const educationItem = screen.getByTestId("education-item");
-    const image = educationItem.querySelector("header > img");
-    const title = educationItem.querySelector("header > h2");
-    const titleSpan = educationItem.querySelector("header > h2 > span");
-    const desc = educationItem.querySelector("section > h3");
-    const firstSubject = educationItem.querySelector(
-      "section:nth-of-type(2) > ul > li"
-    );
-
-    expect(educationItem).toBeInTheDocument();
-    expect(image?.getAttribute("src")).toContain(schoolId);
-    expect(image?.getAttribute("alt")).toContain(schoolName);
-    expect(title?.innerHTML).toContain(
-      educationStrings.items[schoolId].fieldOfStudy
-    );
-    expect(title?.innerHTML).toContain(schoolName);
-    expect(titleSpan?.innerHTML).toContain(years);
-    expect(desc?.innerHTML).toContain(educationStrings.items[schoolId].degree);
-    expect(firstSubject?.innerHTML).toContain(
-      educationStrings.items[schoolId].subjects[0]
-    );
+    expect(container).toMatchSnapshot();
   });
 });
--- app/[lang]/(pages)/recommendations/_components/RecommendationItem.test.tsx
+++ app/[lang]/(pages)/recommendations/_components/RecommendationItem.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import RecommendationItem from "./RecommendationItem";
 
@@ -11,7 +11,7 @@ describe("RecommendationItem component", () => {
   it("renders the component", () => {
     const { authorId, authorName, company } = recommendations[0];
 
-    render(
+    const { container } = render(
       <RecommendationItem
         authorId={authorId}
         authorName={authorName}
@@ -22,24 +22,6 @@ describe("RecommendationItem component", () => {
       />
     );
 
-    const recommendationItem = screen.getByTestId("recommendation-item");
-    const image = recommendationItem.querySelector("header > img");
-    const title = recommendationItem.querySelector("header > div > h2");
-    const subtitle = recommendationItem.querySelector("header > div > h3");
-    const firstParagraph = recommendationItem.querySelector(
-      "section > blockquote"
-    );
-
-    expect(recommendationItem).toBeInTheDocument();
-    expect(image?.getAttribute("src")).toContain(authorId);
-    expect(image?.getAttribute("alt")).toContain(authorName);
-    expect(title?.innerHTML).toContain(authorName);
-    expect(subtitle?.innerHTML).toContain(
-      recommendationsStrings.items[authorId].role
-    );
-    expect(subtitle?.innerHTML).toContain(company);
-    expect(firstParagraph?.innerHTML).toContain(
-      recommendationsStrings.items[authorId].paragraphs[0]
-    );
+    expect(container).toMatchSnapshot();
   });
 });
