d2b539a6ef2379c381f0917532c5e2add10fa6ab
Daniel Xavier <dxdanielxavier@gmail.com>
2024-12-09 08:00:30 -0300

    feat: changed skills to focus on hard skills

--- app/[lang]/resume/_config.ts
+++ app/[lang]/resume/_config.ts
@@ -1,11 +1,19 @@
 export const maxSummaryParagraphs = 3;
 
-export const chosenHardSkills = ["react", "ts", "next", "node", "js", "aws"];
+export const chosenHardSkills = [
+  "react",
+  "ts",
+  "next",
+  "node",
+  "js",
+  "aws",
+  "tailwind",
+  "contentful",
+  "docker",
+];
 
 export const chosenSoftSkills = [
   "leadership",
   "stakeholder-management",
   "problem-solving",
-  "conflict-resolution",
-  "communication",
 ];
