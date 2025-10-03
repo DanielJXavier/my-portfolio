c11662c78674b144c4be06f390c765ecae0de27c
Daniel Xavier <dxdanielxavier@gmail.com>
2025-10-03 11:06:33 -0300

    tests: updated some snapshots

--- app/[lang]/resume/_components/ContactInfo.test.tsx
+++ app/[lang]/resume/_components/ContactInfo.test.tsx
@@ -4,6 +4,12 @@ import { render } from "@testing-library/react";
 
 import ContactInfo from "./ContactInfo";
 
+import { useParams } from "next/navigation";
+
+jest.mock("next/navigation");
+
+(useParams as jest.Mock).mockImplementation(() => ({ lang: "en" }));
+
 describe("ContactInfo component", () => {
   it("renders the component", () => {
     const { container } = render(<ContactInfo />);
